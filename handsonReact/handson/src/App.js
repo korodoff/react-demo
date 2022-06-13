11f3c645abc8bfe8892c81a07798e6768f291484
korodoff <rudoffsutnga368@gmail.com>
2022-06-13 17:46:55 +0530

    first handson usnig function and class component

---
+++ handsonReact/handson/src/App.js
@@ -0,0 +1,39 @@
+import "./component/handsOn1/style.css"
+import './App.css';
+import FunctionComponent from "./component/handsOn1/functionComponent";
+import React, {useState} from "react";
+import ClassComponent from "./component/handsOn1/classComponent";
+
+
+function App() {
+
+  const [show, setShow] = useState(false)
+  const[showSecond, setShowSecond] = useState(false)
+  return (
+    <div className="App">
+      <h3>styling using functional and class component</h3>
+      <div className="flex">
+        
+         <div className="both" onClick={()=>setShow(!show)}>to style using function component</div>
+         <div className='class both' onClick={()=>setShowSecond(!showSecond) }>to see style usnig class component</div>
+      </div>
+      <div>
+
+        <div className="functiontoggle">
+          {
+            show?<div className="togglefirst">
+              <FunctionComponent/>  
+            </div>:null  }
+            
+            {
+            showSecond?<div className="togglefirst">
+              <ClassComponent/>
+            </div>:null  }
+        </div>       
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
